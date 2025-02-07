9c755761da5514f817bf6a52e95120e936f8677c
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2025-02-07 20:37:12 +0500

    new changes

--- school_management/school_management/settings.py
+++ school_management/school_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0wiq9jrk+)82ny5-^+)x#fue^2j(clfgid)tjq38ek)sbn#pvs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
