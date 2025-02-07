58c5ced65eb8d4fc12fffc74cecf90d0fa610367
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2025-02-07 20:10:29 +0500

    add static

--- school_management/school_management/settings.py
+++ school_management/school_management/settings.py
@@ -115,8 +115,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+import os 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# For production, you might want to define STATIC_ROOT for serving static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
